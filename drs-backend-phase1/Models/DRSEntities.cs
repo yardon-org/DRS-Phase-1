14b4912dbe9dce7644dd0b73a31e6ea2f1050c52
Peter <pfoster1@NOR-IC24-01558>
2017-07-11 15:08:49 +0100

    Added Email Controller

---
+++ drs-backend-phase1/Models/DRSEntities.cs
@@ -0,0 +1,19 @@
+﻿namespace drs_backend_phase1.Models
+{
+    /// <summary>
+    /// Partial Class for DRSEntities
+    /// </summary>
+    /// <seealso cref="System.Data.Entity.DbContext" />
+    public partial class DRSEntities
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DRSEntities"/> class.
+        /// </summary>
+        public DRSEntities()
+            : base("name=DRSEntities")
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+    }
+}
