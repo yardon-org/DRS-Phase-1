fbf36f528a587b1a5ac40850e3d8f78dbc4f9f2f
Peter <pfoster1@NOR-IC24-01558>
2017-07-18 11:39:28 +0100

    Fix to Paging

--- drs-backend-phase1/Extensions/Extensions.cs
+++ drs-backend-phase1/Extensions/Extensions.cs
@@ -19,8 +19,12 @@ namespace drs_backend_phase1.Extensions
         public static object DoPaging(this IOrderedQueryable<object> query, int page, int pageSize)
         {
             if (pageSize > 200) pageSize = 200; // Correct for too large a pagesize
+            if (page == 0) page = 1;
+            page -=1;
             var totalCount = query.Count();
             var totalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+
+
 
             var results = query
                 .Skip(pageSize * page)
@@ -29,9 +33,10 @@ namespace drs_backend_phase1.Extensions
 
             return new
             {
-                TotalCount = totalCount,
                 TotalPages = totalPages,
-                PageSize = pageSize,
+                TotalCount = totalCount,
+                CurrentPage = page+1,
+                CountPerPage = pageSize,
                 Results = results
             };
         }
