cc0e4e7099f04d8c4ac059c5476dd7dae1683c65
Bernie Perrett <bernie.perrett@ic24.nhs.net>
2017-07-27 10:13:01 +0100

    Added unit test project

---
+++ DRS-Phase1-UnitTests/ProfileControllerUnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DRS_Phase1_UnitTests
+{
+    [TestClass]
+    public class ProfileControllerUnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
