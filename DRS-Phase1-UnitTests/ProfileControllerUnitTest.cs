f42f43d85f8e489f402e9f7a5bac07c12e3a2e1c
Bernie Perrett <bernie.perrett@ic24.nhs.net>
2017-08-02 13:11:03 +0100

    Added more unit tests

--- DRS-Phase1-UnitTests/ProfileControllerUnitTest.cs
+++ DRS-Phase1-UnitTests/ProfileControllerUnitTest.cs
@@ -63,6 +63,7 @@ namespace DRS_Phase1_UnitTests
             {
                 new ProfileProfessional
                 {
+                    //  0
                     agencyId = 1,
                     dateCreated = DateTime.Now,
                     gmcNumber="G12345678",
@@ -81,6 +82,7 @@ namespace DRS_Phase1_UnitTests
                 },
                 new ProfileProfessional
                 {
+                    //  1
                     agencyId = 1,
                     dateCreated = DateTime.Now,
                     gmcNumber="G12345679",
@@ -99,6 +101,7 @@ namespace DRS_Phase1_UnitTests
                 },
                 new ProfileProfessional
                 {
+                    //  2
                     agencyId = 1,
                     dateCreated = DateTime.Now,
                     gmcNumber="G12345680",
@@ -117,6 +120,7 @@ namespace DRS_Phase1_UnitTests
                 },
                 new ProfileProfessional
                 {
+                    //  3
                     agencyId = 2,
                     dateCreated = DateTime.Now,
                     gmcNumber="G12345681",
@@ -135,6 +139,7 @@ namespace DRS_Phase1_UnitTests
                 },
                 new ProfileProfessional
                 {
+                    //  4
                     agencyId = 2,
                     dateCreated = DateTime.Now,
                     gmcNumber="G12345682",
@@ -143,7 +148,7 @@ namespace DRS_Phase1_UnitTests
                     baseId=1,
                     indemnityExpiryDate = new DateTime(2018,4,14),
                     registeredSurgeryId = 2,
-                    teamId = 2,
+                    teamId = 3,
                     registrarLevelId= 2,
                     nmcNumber = "nmc005",
                     dateModified = DateTime.Now,
@@ -157,6 +162,7 @@ namespace DRS_Phase1_UnitTests
             {
                 new Profile
                 {
+                    //  0
                     address1 = "1 Fake Street",
                     address2 = "FakeVille",
                     address4 = "FakeShire",
@@ -164,6 +170,7 @@ namespace DRS_Phase1_UnitTests
                     dateOfBirth = new DateTime(1960, 1, 1),
                     firstName = "Fred",
                     lastName = "Bloggs",
+                    middleNames = "",
                     homeEmail = "me@me.com",
                     postcode = "PO5 T00",
                     id = 1,
@@ -171,6 +178,7 @@ namespace DRS_Phase1_UnitTests
                 },
                 new Profile
                 {
+                    //  1
                     address1 = "2 Fake Street",
                     address2 = "FakeVille",
                     address4 = "FakeShire",
@@ -178,6 +186,7 @@ namespace DRS_Phase1_UnitTests
                     dateOfBirth = new DateTime(1961, 1, 1),
                     firstName = "Freddy",
                     lastName = "Bloggs",
+                    middleNames = "Percy",
                     homeEmail = "him@me.com",
                     postcode = "PO5 T01",
                     id = 2,
@@ -185,6 +194,7 @@ namespace DRS_Phase1_UnitTests
                 },
                 new Profile
                 {
+                    //  2
                     address1 = "3 Fake Street",
                     address2 = "FakeVille",
                     address4 = "FakeShire",
@@ -192,6 +202,7 @@ namespace DRS_Phase1_UnitTests
                     dateOfBirth = new DateTime(1962, 1, 1),
                     firstName = "Freda",
                     lastName = "Bloggs",
+                    middleNames = "",
                     homeEmail = "her@me.com",
                     postcode = "PO5 T02",
                     id = 3,
@@ -199,6 +210,7 @@ namespace DRS_Phase1_UnitTests
                 },
                 new Profile
                 {
+                    //  3
                     address1 = "4 Fake Street",
                     address2 = "FakeVille",
                     address4 = "FakeShire",
@@ -206,11 +218,28 @@ namespace DRS_Phase1_UnitTests
                     dateOfBirth = new DateTime(1963, 1, 1),
                     firstName = "Deleted",
                     lastName = "Bloggs",
+                    middleNames = "",
                     homeEmail = "deleted@me.com",
-                    postcode = "PO5 T02",
+                    postcode = "PO5 T0",
                     id = 4,
                     isDeleted = true,
                     ProfileProfessional = _testProfessional[3]
+                },
+                new Profile
+                {
+                    //  4
+                    address1 = "2 Fake Street",
+                    address2 = "FakeVille",
+                    address4 = "FakeShire",
+                    dateCreated = DateTime.Now,
+                    dateOfBirth = new DateTime(1964, 1, 1),
+                    firstName = "Fred",
+                    lastName = "Baker",
+                    middleNames = "Percy",
+                    homeEmail = "baker@me.com",
+                    postcode = "PO5 T04",
+                    id = 5,
+                    ProfileProfessional = _testProfessional[4]
                 }
             };
         }
@@ -219,7 +248,9 @@ namespace DRS_Phase1_UnitTests
         public void ProfileSetUp()
         {
             _mockContext = new Mock<DRSEntities>();
-            _testProfileList[2].isDeleted = false;
+            //  The following line is necessary to reset the deleted flag if the "DeleteProfile_Sets_Deleted_Flag_on_Profile" is run before others
+            _testProfileList[1].isDeleted = false;
+
             _queryProfileList = _testProfileList.AsQueryable();
             _queryProfessionalList = _testProfessional.AsQueryable();
         }
@@ -228,98 +259,73 @@ namespace DRS_Phase1_UnitTests
         [TestMethod]
         public void ReturnAllProfiles_Excludes_Deleted()
         {
-            var mockSet = new Mock<DbSet<Profile>>();
-
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.Provider).Returns(_queryProfileList.Provider);
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.Expression).Returns(_queryProfileList.Expression);
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.ElementType).Returns(_queryProfileList.ElementType);
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.GetEnumerator()).Returns(_queryProfileList.GetEnumerator);
-
-            _mockContext.Setup(c => c.Profiles).Returns(mockSet.Object);
+            //   Arrange
+            SetUpProfileMock();
 
             var p = new ProfileController(_mockContext.Object);
 
+            //  Act
             dynamic actionResult = p.FetchAllProfiles();
             dynamic content = actionResult.Content;
 
             // Assert
-
-            Assert.AreEqual(3, content.items.Count);
+            Assert.AreEqual(4, content.items.Count);
 
          }
 
         [TestMethod]
         public void ReturnAllProfiles_Includes_Deleted()
         {
-            var mockSet = new Mock<DbSet<Profile>>();
-
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.Provider).Returns(_queryProfileList.Provider);
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.Expression).Returns(_queryProfileList.Expression);
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.ElementType).Returns(_queryProfileList.ElementType);
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.GetEnumerator()).Returns(_queryProfileList.GetEnumerator);
-
-            _mockContext.Setup(c => c.Profiles).Returns(mockSet.Object);
+            //   Arrange
+            SetUpProfileMock();
 
             var p = new ProfileController(_mockContext.Object);
 
+            //  Act
             dynamic actionResult = p.FetchAllProfiles(true);
             dynamic content = actionResult.Content;
 
             // Assert
-
-            Assert.AreEqual(4, content.items.Count);
+            Assert.AreEqual(5, content.items.Count);
 
         }
 
         [TestMethod]
         public void FetchProfileById_Returns_Correct_Profile()
         {
-            var mockSet = new Mock<DbSet<Profile>>();
-
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.Provider).Returns(_queryProfileList.Provider);
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.Expression).Returns(_queryProfileList.Expression);
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.ElementType).Returns(_queryProfileList.ElementType);
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.GetEnumerator()).Returns(_queryProfileList.GetEnumerator);
-
-            _mockContext.Setup(c => c.Profiles).Returns(mockSet.Object);
+            //   Arrange
+            SetUpProfileMock();
 
             var p = new ProfileController(_mockContext.Object);
 
+            //  Act
             var actionResult = p.FetchProfileById(2);
             var contentResult = actionResult as OkNegotiatedContentResult<ProfileDTO>;
 
-            //dynamic content = actionResult.Content;
-
             // Assert
-
             Assert.IsInstanceOfType(contentResult.Content, typeof(ProfileDTO));
 
             Assert.AreEqual("Freddy", contentResult.Content.firstName);
             Assert.AreEqual(2, contentResult.Content.id);
         }
         [TestMethod]
-        public void DeleteProfile_Removes_Profile()
+        public void DeleteProfile_Sets_Deleted_Flag_on_Profile()
         {
-
-            var mockSet = new Mock<DbSet<Profile>>();
-
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.Provider).Returns(_queryProfileList.Provider);
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.Expression).Returns(_queryProfileList.Expression);
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.ElementType).Returns(_queryProfileList.ElementType);
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.GetEnumerator()).Returns(_queryProfileList.GetEnumerator);
-
-            _mockContext.Setup(c => c.Profiles).Returns(mockSet.Object);
+            //  Arrange
+            var mockSet = SetUpProfileMock();
 
             var p = new ProfileController(_mockContext.Object);
 
+            //  Act
             var actionResult = p.DeleteProfileById(2);
             var contentResult = actionResult as OkNegotiatedContentResult<Boolean>;
 
+            //  Assert
             Assert.AreEqual(true, contentResult.Content);
 
             bool prof = (from fp in mockSet.Object
-                       where fp.id == 2
-                       select fp.isDeleted).Single();
+                           where fp.id == 2
+                           select fp.isDeleted).Single();
 
             Assert.AreEqual(true, prof);
         }
@@ -327,33 +333,119 @@ namespace DRS_Phase1_UnitTests
         [TestMethod]
         public void FetchManyByTeamId_Returns_Many()
         {
-            var mockSet = new Mock<DbSet<Profile>>();
-
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.Provider).Returns(_queryProfileList.Provider);
-            //mockSet.As<IQueryable<ProfileProfessional>>().Setup(m => m.Provider).Returns(_queryProfessionalList.Provider);
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.Expression).Returns(_queryProfileList.Expression);
-            //mockSet.As<IQueryable<ProfileProfessional>>().Setup(m => m.Expression).Returns(_queryProfessionalList.Expression);
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.ElementType).Returns(_queryProfileList.ElementType);
-            //mockSet.As<IQueryable<ProfileProfessional>>().Setup(m => m.ElementType).Returns(_queryProfessionalList.ElementType);
-            mockSet.As<IQueryable<Profile>>().Setup(m => m.GetEnumerator()).Returns(_queryProfileList.GetEnumerator);
-            //mockSet.As<IQueryable<ProfileProfessional>>().Setup(m => m.GetEnumerator()).Returns(_queryProfessionalList.GetEnumerator());
-
-            _mockContext.Setup(c => c.Profiles).Returns(mockSet.Object);
+            //  Arrange
+            SetUpProfileMock();
 
             var p = new ProfileController(_mockContext.Object);
 
-            p.FetchManyByTeamId(2);
-
-            dynamic actionResult = p.FetchAllProfiles(true);
+            //  Act
+            dynamic actionResult = p.FetchManyByTeamId(1);
             dynamic content = actionResult.Content;
 
             // Assert
-
             Assert.AreEqual(3, content.items.Count);
 
+        }
+
+        [TestMethod]
+        public void FetchManyByTeamId__Does_Not_Return_Deleted()
+        {
+            //  Arrange
+
+            SetUpProfileMock();
+
+            var p = new ProfileController(_mockContext.Object);
+
+            //  Act
+            dynamic actionResult = p.FetchManyByTeamId(2);
+            dynamic content = actionResult.Content;
+
+            // Assert
+            Assert.AreEqual(0, content.items.Count);
+        }
+
+        [TestMethod]
+        public void SearchProfiles_Returns_All_Profiles_With_SpecifiedFisrtname()
+        {
+            //  Arrange
+
+            SetUpProfileMock();
+
+            var p = new ProfileController(_mockContext.Object);
+
+            //  Act
+            dynamic actionResult = p.SearchProfiles("Fred");
+            dynamic content = actionResult.Content;
+
+            // Assert
+            Assert.AreEqual(4, content.items.Count);
+        }
+        [TestMethod]
+        public void SearchProfiles_Returns_All_Profiles_With_SpecifiedLastname()
+        {
+            //  Arrange
+
+            SetUpProfileMock();
+
+            var p = new ProfileController(_mockContext.Object);
+
+            //  Act
+            dynamic actionResult = p.SearchProfiles("Bloggs");
+            dynamic content = actionResult.Content;
+
+            // Assert
+            Assert.AreEqual(3, content.items.Count);
+        }
+        [TestMethod]
+        public void SearchProfiles_Returns_All_Profiles_With_SpecifiedLastname_Including_Deleted()
+        {
+            //  Arrange
+
+            SetUpProfileMock();
+
+            var p = new ProfileController(_mockContext.Object);
+
+            //  Act
+            dynamic actionResult = p.SearchProfiles("Bloggs", true);
+            dynamic content = actionResult.Content;
+
+            // Assert
+            Assert.AreEqual(4, content.items.Count);
+        }
+        [TestMethod]
+        public void SearchProfiles_Returns_All_Profiles_With_SpecifiedMiddlename()
+        {
+            //  Arrange
+
+            SetUpProfileMock();
+
+            var p = new ProfileController(_mockContext.Object);
+
+            //  Act
+            dynamic actionResult = p.SearchProfiles("Percy");
+            dynamic content = actionResult.Content;
+
+            // Assert
+            Assert.AreEqual(2, content.items.Count);
         }
         #endregion
         #region "EMail Controller"
         #endregion
+
+        #region "Test Helpers"
+        private Mock<DbSet<Profile>> SetUpProfileMock()
+        {
+            var mockSet = new Mock<DbSet<Profile>>();
+
+            mockSet.As<IQueryable<Profile>>().Setup(m => m.Provider).Returns(_queryProfileList.Provider);
+            mockSet.As<IQueryable<Profile>>().Setup(m => m.Expression).Returns(_queryProfileList.Expression);
+            mockSet.As<IQueryable<Profile>>().Setup(m => m.ElementType).Returns(_queryProfileList.ElementType);
+            mockSet.As<IQueryable<Profile>>().Setup(m => m.GetEnumerator()).Returns(_queryProfileList.GetEnumerator);
+
+            _mockContext.Setup(c => c.Profiles).Returns(mockSet.Object);
+
+            return mockSet;
+        }
+        #endregion  
     }    
 }
