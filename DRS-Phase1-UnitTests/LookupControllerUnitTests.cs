2c475a159c0d79b22db00d6a4bef035adeeddbae
Bernie Perrett <bernie.perrett@ic24.nhs.uk>
2017-08-03 18:01:35 +0100

    Added lookup unit test

--- DRS-Phase1-UnitTests/LookupControllerUnitTests.cs
+++ DRS-Phase1-UnitTests/LookupControllerUnitTests.cs
@@ -1,11 +1,59 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using drs_backend_phase1.Models;
+using drs_backend_phase1.Controllers;
+using System.Web.Http.Results;
+using System.Linq;
+using Moq;
+using System.Data.Entity;
+using AutoMapper;
+using Profile = drs_backend_phase1.Models.Profile;
+using drs_backend_phase1.Models.DTOs;
 
 namespace DRS_Phase1_UnitTests
 {
     [TestClass]
-    public class UnitTest1
+    public class LookupControllerUnitTests
     {
+        [ClassInitialize]
+        public static void ProfileInitialize(TestContext context)
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMissingTypeMaps = true;
+                cfg.CreateMap<Profile, ProfileDTO>();
+                cfg.CreateMap<ProfileProfessional, ProfileProfessionalDTO>();
+                cfg.CreateMap<List<Profile>, List<ProfileDTO>>();
+                cfg.CreateMap<SecurityRole, SecurityRoleDTO>();
+                cfg.CreateMap<CCG, CCGDTO>();
+                cfg.CreateMap<RegistrarLevel, RegistrarLevelDTO>();
+                cfg.CreateMap<Base, BaseDTO>();
+                cfg.CreateMap<Team, TeamDTO>();
+                cfg.CreateMap<IndemnityProvider, IndemnityProviderDTO>();
+                cfg.CreateMap<RegisteredSurgery, RegisteredSurgeryDTO>();
+                cfg.CreateMap<Agency, AgencyDTO>();
+                cfg.CreateMap<JobType, JobTypeDTO>();
+                cfg.CreateMap<ProfileDocument, ProfileDocumentDTO>();
+                cfg.CreateMap<ProfileShiftType, ProfileShiftTypeDTO>();
+                cfg.CreateMap<ShiftType, ShiftTypeDTO>();
+                cfg.CreateMap<SpecialNote, SpecialNoteDTO>();
+                cfg.CreateMap<ProfileFinance, ProfileFinanceDTO>();
+                cfg.CreateMap<ProfilePaymentCategory, ProfilePaymentCategoryDTO>();
+                cfg.CreateMap<PaymentCategory, PaymentCategoryDTO>();
+                cfg.CreateMap<DocumentType, DocumentTypeDTO>();
+                cfg.CreateMap<Bank, BankDTO>();
+                cfg.CreateMap<SecurityPermission, SecurityPermissionDTO>();
+                cfg.CreateMap<SecurityGroup, SecurityGroupDTO>();
+                cfg.CreateMap<SecurityRight, SecurityRightDTO>();
+
+            });
+
+            Mapper.AssertConfigurationIsValid();
+
+            list<alert
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
